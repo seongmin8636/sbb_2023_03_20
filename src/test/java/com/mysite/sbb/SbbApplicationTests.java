b3e651886d78a79399a2df7155ec983fee77a85f
seongmin8636 <moss3004@gmail.com>
2023-03-20 16:38:32 +0900

    데이터 저장

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -28,4 +32,64 @@ class SbbApplicationTests {
 		questionRepository.save(q2);  // 두번째 질문 저장
 	}
 
+	/*
+    SQL
+    SELECT * FROM question
+    */
+	@Test
+	@DisplayName("findAll")
+	void t002() {
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	/*
+    SQL
+    SELECT *
+    FROM question
+    WHERE id = 1
+    */
+	@Test
+	@DisplayName("findById")
+	void t003() {
+		Optional<Question> oq = questionRepository.findById(1);
+
+		if (oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	/*
+    SQL
+    SELECT *
+    FROM question
+    WHERE subject = 'sbb가 무엇인가요?'
+    */
+	@Test
+	@DisplayName("findBySubject")
+	void t004() {
+		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	@DisplayName("findBySubjectAndContent")
+	void t005() {
+		Question q = questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다."
+		);
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	@DisplayName("findBySubjectAndContent")
+	void t006() {
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
 }
